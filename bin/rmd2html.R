8a5074aaec41027dcbf35d2348edc4b42de2eea4
benselby <Ben.Selby@camh.ca>
2021-04-27 11:10:51 -0400

    Auto-updating recruitment doc

--- bin/rmd2html.R
+++ bin/rmd2html.R
@@ -4,7 +4,7 @@ if (length(args)!=2) {
   stop("Both MRI and demographics files are required to render the report..n", call.=FALSE)
 }
 
-Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+Sys.setenv(RSTUDIO_PANDOC="/mnt/tigrlab/quarantine/rstudio/1.1.447/build/rstudio-1.1.447/bin/pandoc")
 Sys.getenv("RSTUDIO_PANDOC")
 rmarkdown::render('OPTIMUM-Neuro_tracking.Rmd', 'html_document', 
 			params=list(mri_file=args[1], demo_file=args[2]),
